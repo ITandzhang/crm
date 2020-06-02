fea64c1fddcc9aee1293e5bae06ac04da1f63ae2
ITandZhang <1714532346@qq.com>
2020-06-02 20:48:32 +0800

    crm第一天 用户登录&用户退出&非法请求&全局异常功能实现

--- src/main/java/com/shsxt/crm/controller/UserController.java
+++ src/main/java/com/shsxt/crm/controller/UserController.java
@@ -1,13 +1,20 @@
 package com.shsxt.crm.controller;
 
 import com.shsxt.base.BaseController;
+import com.shsxt.crm.exceptions.ParamsException;
+import com.shsxt.crm.model.ResultInfo;
+import com.shsxt.crm.model.UserModel;
+import com.shsxt.crm.utils.LoginUserUtil;
 import com.shsxt.crm.vo.User;
 import com.shsxt.crm.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class UserController extends BaseController{
@@ -19,5 +26,19 @@ public class UserController extends BaseController{
     @ResponseBody
     public User queryUserByUserId(Integer userId){
         return userService.selectByPrimaryKey(userId);
+    }
+
+    @RequestMapping("user/login")
+    @ResponseBody
+    public ResultInfo login(String userName,String userPwd){
+        UserModel userModel = userService.login(userName,userPwd);
+        return success("用户登录成功",userModel);
+    }
+
+    @RequestMapping("user/updatePassword")
+    @ResponseBody
+    public ResultInfo updatePassword(HttpServletRequest request,String oldPassword,String newPassword,String confirmPassword){
+        userService.updateUserPassword(LoginUserUtil.releaseUserIdFromCookie(request),oldPassword,newPassword,confirmPassword);
+        return success("密码更新成功");
     }
 }
